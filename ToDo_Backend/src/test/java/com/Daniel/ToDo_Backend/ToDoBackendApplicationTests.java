424496b8de826bad192e58ebdc18c405495ec9fa
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-03-27 13:11:02 +0100

    Backend initial files - spring initializr

---
+++ ToDo_Backend/src/test/java/com/Daniel/ToDo_Backend/ToDoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Daniel.ToDo_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
