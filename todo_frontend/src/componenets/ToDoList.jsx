58326cfeeac9ae5e10cee8d71ab07dc897f5823e
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-03-28 12:04:28 +0100

    To do List component

---
+++ todo_frontend/src/componenets/ToDoList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ToDoList = () => {
+  return (
+    <>
+        <div className="container">
+            <div className="listHeader">
+                <h1>To Do List</h1>
+            </div>
+            <div className="listBody">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default ToDoList
